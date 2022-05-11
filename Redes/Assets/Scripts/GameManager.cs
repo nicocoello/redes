2d7a7d461224b82bbeac0e08eb6774fcc15e0f1f
nicocoello <coellonicolas97@gmail.com>
2022-05-11 16:21:08 -0300

    Timer Added :)

--- Redes/Assets/Scripts/GameManager.cs
+++ Redes/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 
@@ -9,32 +10,47 @@ public class GameManager : MonoBehaviourPunCallbacks
     float timeToStart = 2f;
     public int playersAmount;
     public GameObject wall;
-    
+    //Countdown
+    float _currentTime;
+    public float startingTime = 10f;
+    public Text countDownText;
+
+
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         if (PhotonNetwork.IsMasterClient)
         {
             var playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
             if (playerCount >= playersAmount)
-            {
-                //Inicio la corrutina
-                StartCoroutine(WaitToStart());
+            {                
+                photonView.RPC("StartGame", RpcTarget.All);                
+                photonView.RPC("Start", RpcTarget.All);
+                photonView.RPC("Update", RpcTarget.All);
                 //Si el master client ya comenzo el juego dejo la sala cerrada
                 PhotonNetwork.CurrentRoom.IsOpen = false;
                 PhotonNetwork.CurrentRoom.IsVisible = false;
-            }
+            }           
         }
-        
-    }   
-    IEnumerator WaitToStart()
-    {
-        //Luego del time to start arranca el juego
-        yield return new WaitForSeconds(timeToStart);
-        photonView.RPC("StartGame", RpcTarget.All);
-    }
+    }    
     [PunRPC]
     void StartGame()
     {
-        Destroy(wall);
+        Destroy(wall);        
+    }
+    [PunRPC]
+    private void Start()
+    {       
+        _currentTime = startingTime;     
+    }
+    [PunRPC]
+    private void Update()
+    {
+        _currentTime -= 1 * Time.deltaTime;
+        countDownText.text = _currentTime.ToString("0");
+        if (_currentTime <= 0)
+        {
+            _currentTime = 0;
+            Time.timeScale = 0f;
+        }
     }
 }
