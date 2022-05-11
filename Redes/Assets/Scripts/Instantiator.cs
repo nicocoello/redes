839a77adb9a192b6ee30d7fbfd7818c544f69de8
nicocoello <coellonicolas97@gmail.com>
2022-05-10 23:06:28 -0300

    Half Camera Follow

--- Redes/Assets/Scripts/Instantiator.cs
+++ Redes/Assets/Scripts/Instantiator.cs
@@ -8,13 +8,16 @@ public class Instantiator : MonoBehaviour
 {
     public Transform point;
     public Transform spawnPoint;
+    public Transform playerPos;
     public string prefabName;
     void Start()
     {
         //Instancio los diferentes prefabs de la carpeta resources
        var obj = PhotonNetwork.Instantiate(prefabName,spawnPoint.position,Quaternion.identity);
-       var nick = PhotonNetwork.Instantiate("PlayerNickName", point.position,point.rotation);              
+       var nick = PhotonNetwork.Instantiate("PlayerNickName", point.position,point.rotation);
+       var cam = PhotonNetwork.Instantiate("Camera",playerPos.position,playerPos.rotation);
        nick.GetComponent<PlayerNickNames>().SetNick(PhotonNetwork.LocalPlayer.NickName,obj);
+       cam.GetComponent<CameraFollow>().SetFollow(obj);
     }
    
 }
