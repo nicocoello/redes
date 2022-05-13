a64f9b60527b2e6c60e2bcddaae835795ca3f17a
nicocoello <coellonicolas97@gmail.com>
2022-05-13 15:41:33 -0300

    Erased some stuff && New DeathFloor

---
+++ Redes/Assets/Scripts/TriggerEvents/BadFloor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class BadFloor : MonoBehaviourPun
+{
+    public Character _char;
+    public Transform respawn;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var character = collision.GetComponent<Character>();
+        _char = character;
+        if (PhotonNetwork.IsMasterClient)
+        {
+
+
+            if (character != null)
+            {
+                var photonViewCharacter = character.GetComponent<PhotonView>();
+                var Tped = photonViewCharacter.Owner;
+                photonView.RPC("TeleportBack", RpcTarget.All, Tped);
+
+            }
+        }
+    }
+    [PunRPC]
+    void TeleportBack(Player player)
+    {
+        if (player == PhotonNetwork.LocalPlayer)
+        {
+            _char.transform.position = respawn.transform.position;
+        }
+        else
+        {
+            _char.transform.position = respawn.transform.position;
+        }
+    }
+}
